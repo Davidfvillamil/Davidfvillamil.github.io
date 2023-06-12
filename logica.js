b16d34582e278b8e0efd452fe9904d3094e37e1f
David Villamil <davidfvillamilm@gmail.com>
2023-06-11 23:44:25 -0500

    hacer la pagina responsive a 764px, 990px y 1200px

--- logica.js
+++ logica.js
@@ -1,6 +1,28 @@
+// sección para hacer el nav-bar responsive
+
+let icon_menu = document.querySelector('.bx-menu')
+let nav_nar = document.querySelector('.nav-bar')
+
+icon_menu.onclick = () => {
+    
+    
+    
+    
+    if (icon_menu.classList.contains('bx-menu')){
+        icon_menu.classList.replace('bx-menu','bxl-tux')
+    }else{
+        icon_menu.classList.replace('bxl-tux','bx-menu')
+    }
+    nav_nar.classList.toggle('active');
+    
+}
+
+
+
 var sections = document.querySelectorAll('section')
 var nav_links = document.querySelectorAll('header nav a')
 
+// Sección para que cuando haga scrolldown se ilumine en el nagbar la parte correspondiente a la sección en la que estamos 
 window.onscroll = () =>{
     sections.forEach(secuencia => {
         let arriba = window.scrollY
@@ -16,4 +38,11 @@ window.onscroll = () =>{
         }        
     })
 
+    let header = document.querySelector('header')
+
+    header.classList.toggle('sticky', window.scrollY > 100)
+
+    
+
 }
+
